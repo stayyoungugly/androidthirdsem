dc72ede9567d31cd94df8d26ec723e3a0cdd9606
Ildar <ildar.muckhaev@gmail.com>
2021-10-04 21:49:38 +0300

    hw2

--- app/src/main/java/com/example/androidsemthree/MainActivity.kt
+++ app/src/main/java/com/example/androidsemthree/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.androidsemthree
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.start_brown)
+
+        val editButton: Button = findViewById(R.id.edit_button)
+        val nameEt: EditText = findViewById(R.id.et_name)
+        val nameTv: TextView = findViewById(R.id.tv_name)
+
+        var pressed = false;
+        editButton.setOnClickListener {
+            if (!pressed) {
+                nameTv.visibility = INVISIBLE
+                nameEt.visibility = VISIBLE
+                editButton.text = "Сохранить"
+                pressed = true
+            } else {
+                nameEt.visibility = GONE
+                nameTv.text = nameEt.text
+                editButton.text = "Изменить имя"
+                nameTv.visibility = VISIBLE
+                pressed = false
+            }
+        }
     }
 }
